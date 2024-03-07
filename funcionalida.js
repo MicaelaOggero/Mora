e2f62a2fa5e3d6933d0e886dde012c3596aaf588
unknown <micaoggero17@gmail.com>
2024-03-07 14:58:56 -0300

    index, inicio sesion, crear cuenta

---
+++ funcionalida.js
@@ -0,0 +1,60 @@
+var btnAbrirInicio = document.getElementById('btn-abrir-inicio'),
+overlay = document.getElementById('overlay'), 
+popup=document.getElementById('popup'),
+btnCerrarInicio=document.getElementById('btn-cerrar-inicio');
+var btnAbrirCrearCuenta = document.getElementById('btn-abrir-crearCuenta'),
+overlayC = document.getElementById('overlay-c'), 
+popupC=document.getElementById('popup-c'),
+btnCerrarCrearCuenta=document.getElementById('btn-cerrar-crearCuenta');
+
+btnAbrirInicio.addEventListener('click', function(){
+    overlay.classList.add('active-i');
+});
+
+btnCerrarInicio.addEventListener('click', function(){
+    overlay.classList.remove('active-i');
+});
+
+btnAbrirCrearCuenta.addEventListener('click', function(){
+    overlayC.classList.add('active-c');
+});
+
+btnCerrarCrearCuenta.addEventListener('click', function(){
+    overlayC.classList.remove('active-c');
+});
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("abajo",window.scrollY>0);
+});
+
+document.getElementById('toggleCartBtn').addEventListener('click', function() {
+    var cartPanel = document.getElementById('cartPanel');
+    cartPanel.style.right = (cartPanel.style.right === '-400px') ? '0' : '-400px';
+});
+
+document.addEventListener('click', function(event) {
+    var cartPanel = document.getElementById('cartPanel');
+    var target = event.target;
+
+    // Si el clic no está dentro del panel del carrito ni en el botón de Carrito, cerramos el panel
+    if (target !== cartPanel && target !== document.getElementById('toggleCartBtn')) {
+        cartPanel.style.right = '-400px';
+    }
+});
+
+// Obtener referencias a los elementos del DOM
+const toggleButton = document.getElementById('toggleButton');
+const elementoVisible = document.getElementById('toggleCartBtn');
+
+// Agregar un evento de clic al botón
+toggleButton.addEventListener('click', function() {
+    // Verificar si el elemento es visible
+    if (elementoVisible.style.display === 'flex' || elementoVisible.style.display === '') {
+        // Si no es visible, hacerlo visible
+        elementoVisible.style.display = 'none';
+    } else {
+        // Si es visible, ocultarlo
+        elementoVisible.style.display = 'flex';
+    }
+});
